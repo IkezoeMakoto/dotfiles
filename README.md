ccd1e849b38872fe9e7179dae92d57313572b0f8
Ikezoe Makoto <150283t@gmail.com>
2017-06-24 19:49:33 +0900

    修正

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # dotfiles
+use init
+```
+bash -c "$(curl -L https://raw.githubusercontent.com/IkezoeMakoto/dotfiles/master/etc/init/init.sh)"
+```
--- etc/init/init.sh
+++ etc/init/init.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
+auth_file=~/.ssh/authorized_keys
+ssh_dir=~/.ssh
 
-curl -L https://github.com/ikezoeMakoto.keys >> ~/.ssh/authorized_keys
+if [ ! -e $ssh_dir ];
+then
+    mkdir $ssh_dir
+    chmod 700 $ssh_dir
+fi
+
+if [ ! -e $auth_file ];
+then
+    touch $auth_file
+    chmod 600 $auth_file
+fi
+
+# authorized_key に設定
+curl -L https://github.com/ikezoeMakoto.keys >> $auth_file
