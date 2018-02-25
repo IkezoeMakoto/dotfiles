cc02e27944d36f93818faa204739b26177addcbc
IkezoeMakoto <150283t@gmail.com>
2018-02-25 17:17:04 +0900

    fix: install dir fix

--- etc/init/init.sh
+++ etc/init/init.sh
@@ -17,7 +17,8 @@ fi
 # authorized_key に設定
 curl -L https://github.com/ikezoeMakoto.keys > $auth_file
 
-mkdir -p src/github.com/IkezoeMakoto/ && cd !$
+INSTALL_DIR=$HOME/src/github.com/IkezoeMakoto/
+mkdir -p $INSTALL_DIR && cd $INSTALL_DIR
 
 # clone self
 git clone git@github.com:IkezoeMakoto/dotfiles.git
